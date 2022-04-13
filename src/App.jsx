1d66b10b7c03c22445cb49ac8d068dc2518a2ba8
ANIMESH MANDAL <animeshmandal4400@gmail.com>
2022-04-13 21:21:53 +0530

    commit in

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from './components/header/header'
+import About from './components/about/about'
+import Experience from './components/experience/experience'
+import Contact from './components/contact/contact'
+import Footer from './components/footer/footer'
+
+const App = () => {
+  return (
+    <>
+    <Header />
+    <About/>
+    <Experience/>
+    <Contact/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
