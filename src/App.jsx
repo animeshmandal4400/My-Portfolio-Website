9ed937c35e71207224e14891d65b8e3428e4eea7
ANIMESH MANDAL <animeshmandal4400@gmail.com>
2022-04-14 15:19:06 +0530

    commit in

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
-import Header from './components/header/header'
-import About from './components/about/about'
-import Experience from './components/experience/experience'
+import Header from './components/header/header.jsx'
+import About from './components/about/about.jsx'
+import Experience from './components/experience/experience.jsx'
 import Contact from './components/contact/contact'
 import Footer from './components/footer/footer'
 
 const App = () => {
   return (
-    <>
+    <div>
     <Header />
     <About/>
     <Experience/>
     <Contact/>
     <Footer/>
-    </>
+    </div>
   )
 }
 
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import './header.css'
- 
+import CTA from './CTA'
+/*import HeaderSocials from './HeaderSocials'*/
 const header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello, my name is</h5>
+        <h1>Animesh Mandal</h1>
+        <h5 className="text-light">And i'am a Web Developer</h5>
+        <CTA />
+     
+      </div>
+      <div>
+
+      </div>
+    </header>
   )
 }
 
