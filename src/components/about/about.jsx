f4e4114deace7a97eb19e60cd8ddf9f526e74f6a
ANIMESH MANDAL <animeshmandal4400@gmail.com>
2022-04-15 14:43:56 +0530

    commit in

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import './about.css'
-const about = () => {
+import ME from '../../assets/aboutMe.jpg';
+const About = () => {
   return (
-    <section id='about'> about</section>
+    <section id='about'> 
+    <h5>Get to Know</h5>
+    <h2>About Me</h2>
+
+    <div className="container about__container">
+      
+      <div className="about__me">
+      <div className="about__me-image">
+        <img src={ME} alt="about image" />
+      </div>
+      </div>
+      
+    <div className="about__content">
+      
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed odit natus quam nisi enim eaque, perferendis laborum voluptates quod minus dolorem eveniet assumenda pariatur sint exercitationem facere accusamus repellendus at! 
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias tempore ullam nobis aspernatur, aliquid reprehenderit ipsum et sunt! Accusamus unde cumque totam provident dolor voluptatem expedita quia earum nostrum doloremque.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi deserunt ipsum quis ex. Sapiente alias sequi placeat odit laboriosam, error ut ratione saepe aut quas doloremque aspernatur mollitia nesciunt? Explicabo.</p>
+      
+      <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+     </div>
+
+    </div>
+    </section>
+
   )
 }
 
-export default about
+export default About
 
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -7,8 +7,8 @@ const HeaderSocials = () => {
   return (
     <div className='header__socials'>
      <a href="https://www.linkedin.com/in/animesh-mandal-5b55b61a7/" target="_blank"><BsLinkedin/></a>
-     <a href="" target="_blank"><FiGithub/></a>
-     <a href="" target="_blank"><BsTwitter/></a>
+     <a href="https://github.com/animeshmandal4400" target="_blank"><FiGithub/></a>
+     <a href="https://www.instagram.com/_a.n.im.e.s.h_/" target="_blank"><BsTwitter/></a>
      <a href="" target="_blank"><BsInstagram/></a>
     </div>
   )
