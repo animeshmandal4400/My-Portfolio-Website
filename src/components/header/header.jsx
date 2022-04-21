538571b03f3563b3bef62a37352fa00f6ce5976f
ANIMESH MANDAL <animeshmandal4400@gmail.com>
2022-04-21 19:41:48 +0530

    commit in

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,22 +3,33 @@ import './header.css'
 import CTA from './CTA'
 import ME from '../../assets/me.png'
 import HeaderSocials from './HeaderSocials'
-<script src='https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.12/typed.min.js'></script>
+import { Typewriter } from 'react-simple-typewriter'
 
 const header = () => {
   return (
     <header>
       <div className="container header__container">
-        <h5>Hello, i'm</h5>
+        <h5>Hello i'm</h5>
         <h1>Animesh Mandal</h1>
-        <h5 className="text-light">And i'am Web Developer</h5>
+        <h5 className="text-light typing" >And i'am   <span style={{ color: 'white', fontWeight: 'bold' }}>
+          {/* Style will be inherited from the parent element */}
+          <Typewriter
+            words={[ 'Web Developer', 'Game Developer', 'Graphics Designer']}
+            loop={0}
+            cursor
+            cursorStyle='|'
+            typeSpeed={70}
+            deleteSpeed={50}
+            delaySpeed={1000}
+          />
+        </span></h5>
         <CTA />
         <div className="ME">
           <img src={ME} alt="" />
         </div>
         <HeaderSocials/>
         <div>
-        <a href="#footer" className='scroll__down'>Scroll Down</a>
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
       </div>
       </div>
       
